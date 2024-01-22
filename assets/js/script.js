a7a5f5be55c54e51a68fe59f8a881cf352f136d1
Pazoraf <alexander.flaherty@hotmail.com>
2024-01-22 13:57:23 +0000

    Adjusted html bootstrap to better match example image.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,30 @@
 $(document).ready(function() {
-    var currentDay = dayjs().format("MMMM DD YYYY");
-    var currentTime = dayjs();
+    var currentDay = dayjs().format('DD MMM YYYY [at] hh:mm:ss a');
     var currentHour = dayjs().format("H");
-$("#currentDay").text(currentDay);
 
-// $(".timeblock").each(function() {
-//     var blockTime = parseInt($(this).attr("id"));
+    $("#currentDay").text(currentDay);
 
-//     if (blockTime< currentHour){
-//         $(this).addClass("past");
-//     }else if (blockTime==currentHour){
-//         $(this).addClass("present");
-//     }else {
-//         $(this).addClass("future");
-//     }
-// })
+    function timeDisplay(){
+        var currentDay = dayjs().format('DD MMM YYYY [at] hh:mm:ss a');
+        $("#currentDay").text(currentDay);
+    }
 
-// Get the current hour using Day.js
-var currentHour = dayjs().format("H");
+    $(".time-block").each(function() {
+        var blockTime = parseInt($(this).attr("id"));
 
-// Loop through each timeblock
-$(".timeblock").each(function() {
-  // Get the hour of the timeblock
-  var blockHour = parseInt($(this).attr("id"));
-
-  // Compare the blockHour with the currentHour
-  if (blockHour < currentHour) {
-    // Timeblock is in the past
-    $(this).addClass("past");
-  } else if (blockHour == currentHour) {
-    // Timeblock is in the present
-    $(this).addClass("present");
-  } else {
-    // Timeblock is in the future
-    $(this).addClass("future");
-  }
-});
+        if (blockTime< currentHour){
+            $(this).addClass("past");
+        }else if (blockTime==currentHour){
+            $(this).addClass("present");
+        }else {
+            $(this).addClass("future");
+        }
+    })
 
 
 
-console.log("hello")
+
+    setInterval(timeDisplay, 1000)
+    console.log("hello")
 })
 
